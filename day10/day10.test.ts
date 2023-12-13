34282c86f0799bc637b98fb5cb9c30e2df32cb56
Paweł Fronczak <pfronczak@gmail.com>
2023-12-13 23:28:19 +0100

    Day 10, part 2

--- day10/day10.test.ts
+++ day10/day10.test.ts
@@ -7,6 +7,18 @@ const sample = readFileSync('day10/sample.txt', 'utf8')
 const sample2 = readFileSync('day10/sample2.txt', 'utf8')
     .split('\n')
     .map((line) => line.trim());
+const sample3 = readFileSync('day10/sample3.txt', 'utf8')
+    .split('\n')
+    .map((line) => line.trim());
+const sample4 = readFileSync('day10/sample4.txt', 'utf8')
+    .split('\n')
+    .map((line) => line.trim());
+const sample5 = readFileSync('day10/sample5.txt', 'utf8')
+    .split('\n')
+    .map((line) => line.trim());
+const sample6 = readFileSync('day10/sample6.txt', 'utf8')
+    .split('\n')
+    .map((line) => line.trim());
 
 const input = readFileSync('day10/input.txt', 'utf8')
     .split('\n')
@@ -25,11 +37,23 @@ describe('Day 10: Pipe Maze', () => {
         expect(day10_1(input)).toBe(6815);
     });
 
-    // test('Part 2, example 1', () => {
-    //     expect(day10_2(sample)).toBe(0);
-    // });
+    test('Part 2, example 1', () => {
+        expect(day10_2(sample3)).toBe(4);
+    });
 
-    // test('Part 2, puzzle input', () => {
-    //     expect(day10_2(input)).toBe(0);
-    // });
+    test('Part 2, example 2', () => {
+        expect(day10_2(sample4)).toBe(4);
+    });
+
+    test('Part 2, example 3', () => {
+        expect(day10_2(sample5)).toBe(8);
+    });
+
+    test('Part 2, example 4', () => {
+        expect(day10_2(sample6)).toBe(10);
+    });
+
+    test('Part 2, puzzle input', () => {
+        expect(day10_2(input)).toBe(269);
+    });
 });
--- day10/day10.ts
+++ day10/day10.ts
@@ -5,8 +5,9 @@ const PIPE_LINKS = {
     '7': ['D', 'L'],
     L: ['U', 'R'],
     J: ['U', 'L'],
-    '.': []
+    '.': [],
 };
+const DIRECTIONS = ['U', 'R', 'D', 'L'];
 const REVERSE = {
     U: 'D',
     D: 'U',
@@ -18,14 +19,14 @@ const day10_1 = (input: string[]): number => {
     let startX, startY;
     const grid = input.map((line) => line.split(''));
     for (let y = 0; y < grid.length; y++) {
-        for (let x = 0; x < grid.length; x++) {
+        for (let x = 0; x < grid[y].length; x++) {
             if (grid[y][x] === 'S') {
                 startX = x;
                 startY = y;
             }
         }
     }
-    let [currX, currY, dir] = findLinked(grid, startX, startY);
+    let [currX, currY, dir] = findLinked(grid, startX, startY)[0];
     let loopLength = 1;
     while (grid[currY][currX] !== 'S') {
         loopLength++;
@@ -35,31 +36,101 @@ const day10_1 = (input: string[]): number => {
 };
 
 const day10_2 = (input: string[]): number => {
-    return 0;
+    let startX, startY;
+    const grid = input.map((line) => line.split(''));
+    for (let y = 0; y < grid.length && startY == null; y++) {
+        for (let x = 0; x < grid[y].length && startX == null; x++) {
+            if (grid[y][x] === 'S') {
+                startX = x;
+                startY = y;
+            }
+        }
+    }
+    const startNodes = findLinked(grid, startX, startY);
+    let [currX, currY, dir] = startNodes[0];
+    const loopSegments = new Set([`${startX},${startY}`]);
+    let loopSpin = 0;
+    while (grid[currY][currX] !== 'S') {
+        loopSegments.add(`${currX},${currY}`);
+        let prevDir = dir;
+        [currX, currY, dir] = nextTile(grid, currX, currY, dir);
+        let dirDiff =
+            (DIRECTIONS.indexOf(REVERSE[dir]) -
+                DIRECTIONS.indexOf(REVERSE[prevDir]) +
+                4) %
+            4;
+        if (dirDiff === 3) dirDiff = -1;
+        loopSpin += dirDiff;
+    }
+    if (loopSpin < 0) {
+        [currX, currY, dir] = startNodes[1];
+    } else {
+        [currX, currY, dir] = startNodes[0];
+    }
+
+    const insideTiles = new Set();
+    while (grid[currY][currX] !== 'S') {
+        let prevDir = dir,
+            prevX = currX,
+            prevY = currY;
+        [currX, currY, dir] = nextTile(grid, currX, currY, dir);
+        rightRay(prevX, prevY, REVERSE[prevDir], loopSegments).forEach(
+            insideTiles.add,
+            insideTiles
+        );
+        rightRay(prevX, prevY, REVERSE[dir], loopSegments).forEach(
+            insideTiles.add,
+            insideTiles
+        );
+    }
+    console.log(insideTiles);
+    return insideTiles.size;
 };
 
+function rightRay(x, y, dir, loopSegments) {
+    const insideTiles = [];
+    let [currX, currY] = [x, y];
+    const rightDir = DIRECTIONS[(DIRECTIONS.indexOf(dir) + 1) % 4];
+    while (true) {
+        [currX, currY] = move(currX, currY, rightDir);
+        if (!loopSegments.has(`${currX},${currY}`)) {
+            insideTiles.push(`${currX},${currY}`);
+        } else {
+            return insideTiles;
+        }
+    }
+}
+
 function findLinked(grid, x, y) {
+    const startNodes = [];
     if (y > 0 && PIPE_LINKS[grid[y - 1][x]].includes('D'))
-        return [x, y - 1, 'D'];
+        startNodes.push([x, y - 1, 'D']);
     if (x > 0 && PIPE_LINKS[grid[y][x - 1]].includes('R'))
-        return [x - 1, y, 'R'];
-    if (PIPE_LINKS[grid[y][x + 1]].includes('L')) return [x + 1, y, 'L'];
-    if (PIPE_LINKS[grid[y + 1][x]].includes('U')) return [x, y + 1, 'U'];
+        startNodes.push([x - 1, y, 'R']);
+    if (PIPE_LINKS[grid[y][x + 1]].includes('L'))
+        startNodes.push([x + 1, y, 'L']);
+    if (PIPE_LINKS[grid[y + 1][x]].includes('U'))
+        startNodes.push([x, y + 1, 'U']);
+    return startNodes;
 }
 
 function nextTile(grid, x, y, dirFrom) {
     let dir = PIPE_LINKS[grid[y][x]].filter((d) => d !== dirFrom)[0];
+    return [...move(x, y, dir), REVERSE[dir]];
+}
+
+function move(x, y, dir): number[] {
     switch (dir) {
         case 'R':
-            return [x + 1, y, REVERSE[dir]];
+            return [x + 1, y];
         case 'L':
-            return [x - 1, y, REVERSE[dir]];
+            return [x - 1, y];
         case 'U':
-            return [x, y - 1, REVERSE[dir]];
+            return [x, y - 1];
         case 'D':
-            return [x, y + 1, REVERSE[dir]];
+            return [x, y + 1];
     }
-    return [x, y, dirFrom];
+    return [x, y];
 }
 
 export { day10_1, day10_2 };
